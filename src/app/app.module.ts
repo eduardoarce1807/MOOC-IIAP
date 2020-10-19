d8118ee9654e129fedcebd25fd4993ce8b740b54
Eduardo Arce <eduardoarce@upeu.edu.pe>
2020-10-19 18:25:02 -0500

    Actualización de plantilla y componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { PageWrapperComponent } from './components/page-wrapper/page-wrapper.component';
+import { PageContentComponent } from './components/page-content/page-content.component';
+import { SidebarNavComponent } from './components/sidebar-nav/sidebar-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    PageWrapperComponent,
+    PageContentComponent,
+    SidebarNavComponent
   ],
   imports: [
     BrowserModule,
